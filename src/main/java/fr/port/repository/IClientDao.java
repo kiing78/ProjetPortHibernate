b0b902e37bac5098a1efa15db5997c315023061c
kiing78 <kev97250@hotmail.fr>
2023-03-10 15:16:17 +0100

    Revert "Initial commit"
    This reverts commit 36cab5ca6a608e52fcb4675401dca8bce7aa2a61.

--- src/main/java/fr/port/repository/IClientDao.java
+++
@@ -1,9 +0,0 @@
-package fr.port.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fr.port.entity.Client;
-
-public interface IClientDao extends CrudRepository<Client,Long> {
-
-}
--- src/main/java/fr/port/repository/ITransitaire.java
+++
@@ -1,9 +0,0 @@
-package fr.port.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fr.port.entity.Transitaire;
-
-public interface ITransitaire extends CrudRepository<Transitaire,Long>{
-
-}
